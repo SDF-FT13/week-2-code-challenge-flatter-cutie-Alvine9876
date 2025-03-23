db1d8b3c6417198855cc01d2679360fa6cfd35e7
Alvine <alvine.munene@student.moringaschool.com>
2025-03-23 21:09:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function fetchCharacters() {
+    fetch("http://localhost:3000/characters")
+      .then((response) => response.json())
+      .then((characters) => {
+        const characterBar = document.getElementById("character-bar");
+  
+        characters.forEach((character) => {
+          const characterSpan = document.createElement("span");
+          characterSpan.textContent = character.name;
+          characterSpan.addEventListener("click", () => displayCharacterDetails(character));
+  
+          characterBar.appendChild(characterSpan);
+        });
+      })
+      .catch((error) => {
+        console.error("Error fetching characters:", error);
+      });
+  };
+  function displayCharacterDetails(character) {
+    console.log(character);
+  }
